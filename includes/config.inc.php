671fecf6dadd6b6a2a532f8af8d202813695e525
gabor022 <83359693+gabor022@users.noreply.github.com>
2021-04-28 17:50:02 +0200

    Add files via upload

---
+++ includes/config.inc.php
@@ -0,0 +1,40 @@
+<?php
+$ablakcim = array(
+    'cim' => 'A Jövő Öko-Nemzedéke Alapítvány',
+);
+
+$fejlec = array(
+    'kepforras' => 'logo.png',
+    'kepalt' => 'logo',
+	'cim' => 'A Jövő Öko-Nemzedéke Alapítvány',
+	'motto' => ''
+);
+
+$lablec = array(
+    'copyright' => 'Copyright '.date("Y").'.',
+    'ceg' => 'A Jövő Öko-Nemzedéke Alapítvány'
+);
+
+$oldalak = array(
+	'/' => array('fajl' => 'cimlap', 'szoveg' => 'Címlap', 'menun' => array(1,1)),
+	'bemutatkozas' => array('fajl' => 'bemutatkozas', 'szoveg' => 'Bemutatkozás', 'menun' => array(1,1)),
+	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),
+	'uzenetek' => array('fajl' => 'uzenetek', 'szoveg' => 'Üzenetek', 'menun' => array(1,1)),
+	'galeria' => array('fajl' => 'galeria', 'szoveg' => 'Galeria', 'menun' => array(1,1)),
+	'kepfeltoltes' => array('fajl' => 'kepfeltoltes', 'szoveg' => 'Képfeltöltés', 'menun' => array(1,1)),
+	'videok' => array('fajl' => 'videok', 'szoveg' => 'Videok', 'menun' => array(1,1)),
+    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
+    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
+    'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
+    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
+);
+
+$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+$MAPPA = './images/fel/';
+$TIPUSOK = array ('.jpg', '.png');
+$MEDIATIPUSOK = array('image/jpeg', 'image/png');
+$DATUMFORMA = "Y.m.d. H:i";
+$MAXMERET = 500*1024;
+
+
+?>
